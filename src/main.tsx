2464859c2e0f449477c8bab5ce8caaf0f49ac182
Darkmagician0901 <jeff.chang2@ubc.ca>
2026-06-30 15:55:16 -0700

    feat(story): self-host Press Start 2P via @fontsource

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import '@fontsource/press-start-2p/400.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
